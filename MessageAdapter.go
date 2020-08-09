12cacfcb0b2617c812f30a7c55aa9a6de0fa6201
qiqiworld <1354092549@qq.com>
2020-08-09 18:24:28 +0800

    docs: add docs to adapter types

--- MessageAdapter.go
+++ MessageAdapter.go
@@ -1,6 +1,9 @@
 package wsrpc
 
+// MessageAdapter is an adapter for rpc services to read and write messages.
 type MessageAdapter interface {
+	// ReadMessage reads a message. If the connection is closed, this function must return an error.
 	ReadMessage() ([]byte, error)
+	// WriteMessage writes a message. If the connection is closed, this function must return an error.
 	WriteMessage(data []byte) error
 }
--- WebsocketMessageAdapter.go
+++ WebsocketMessageAdapter.go
@@ -2,19 +2,23 @@ package wsrpc
 
 import "github.com/gorilla/websocket"
 
+// WebsocketMessageAdapter is an adapter for rpc services to work with gorilla/websocket
 type WebsocketMessageAdapter struct {
 	conn *websocket.Conn
 }
 
+// NewWebsocketMessageAdapter creates a adapter.
 func NewWebsocketMessageAdapter(conn *websocket.Conn) *WebsocketMessageAdapter {
 	return &WebsocketMessageAdapter{conn: conn}
 }
 
+// ReadMessage reads a message. If the connection is closed, this function must return an error.
 func (a *WebsocketMessageAdapter) ReadMessage() ([]byte, error) {
 	_, message, err := a.conn.ReadMessage()
 	return message, err
 }
 
+// WriteMessage writes a message. If the connection is closed, this function must return an error.
 func (a *WebsocketMessageAdapter) WriteMessage(data []byte) error {
 	return a.conn.WriteMessage(websocket.TextMessage, data)
 }
